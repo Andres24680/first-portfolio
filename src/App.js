111deb000e99f0d5633f15334f262a1dbd9bb568
Andres24680 <tasboyandresrestrepo@aol.com>
2023-02-09 20:18:15 -0500

    made heron section 100vh

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import ResponsiveAppBar from "./components/NavBar"
 import About from './components/About';
 import Contact from './components/Contact';
 import Projects from './components/Projects';
+import Hero from './components/Hero';
+import './App.css'
 
 function App() {
   return(
     <div>
       < ResponsiveAppBar />
+      <Hero />
+      < About />
+      < Projects />
+      <Contact />
     </div>
   )
 }
