518c3f11e899db434f1feb76a1be73b3593007d6
Andres24680 <tasboyandresrestrepo@aol.com>
2023-02-09 20:45:25 -0500

    making homepage component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,14 @@ import Contact from './components/Contact';
 import Projects from './components/Projects';
 import Hero from './components/Hero';
 import './App.css'
+import { BrowserRouter as Route, Link, Routes } from "react-router-dom";
+import Home from './Pages/Home';
 
 function App() {
   return(
     <div>
       < ResponsiveAppBar />
-      <Hero />
-      < About />
-      < Projects />
-      <Contact />
+      < Home />
     </div>
   )
 }
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Projects from '../components/Projects'
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
